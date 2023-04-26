e948edbd08d66387caf9018d14cae8ea78d4ae5c
lukisko <lukas.pleva@mail.com>
2023-04-26 08:00:29 +0200

    create a docker container

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
-# this file is no longer used, use app/__init__.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World2!</p>"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
