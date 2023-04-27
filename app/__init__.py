b5865f177eca28d3a40df9fe2a1dc8f3837e3942
lukisko <lukas.pleva@mail.com>
2023-04-27 09:17:04 +0200

    add interfaces for listing drinks

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+<<<<<<< HEAD
+=======
+
+import app.persistance as pers
+import app.persistance.products as products
+>>>>>>> 0d7f3b2 (add interfaces for listing drinks)
 
 app = Flask(__name__)
 
@@ -10,5 +16,9 @@ def indexPage():
 def productsListPage():
     return render_template('ProductsList.html')
 
+@app.route("/list")
+def getListOfDrinks():
+    return render_template('list.html', list=products.getAllDrinks())
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=5000)
---
+++ app/persistance/products.py
@@ -0,0 +1,3 @@
+
+def getAllDrinks():
+    return [1,2,3,4,5,8,11]
