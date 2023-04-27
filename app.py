4de47c894b4ca9b737e1adf14348ac0d856cb022
sackboynick <nicola.santolini@live.com>
2023-04-27 11:54:58 +0200

    FirstCommit Added FrontPage and First version of ProductsList page.

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def indexPage():
+    return render_template('index.html')
+
+@app.route("/ProductsList")
+def productsListPage():
+    return render_template('ProductsList.html')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=8000,true)
